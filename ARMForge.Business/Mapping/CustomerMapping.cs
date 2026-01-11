dfe8c51d8a9c43e8063730bd45cc05997f1ccef4
galipabay <galipabay@GALIPSSTARSHIP>
2026-01-12 00:10:44 +0300

    refactor(mapping): complete decoupling of AutoMapper profiles into entity-specific classes

---
+++ ARMForge.Business/Mapping/CustomerMapping.cs
@@ -0,0 +1,20 @@
+﻿using ARMForge.Kernel.Entities;
+using ARMForge.Types.DTOs;
+using AutoMapper;
+using System;
+
+namespace ARMForge.Business.Mapping
+{
+    public class CustomerMapping : Profile
+    {
+        public CustomerMapping()
+        {
+            CreateMap<CustomerCreateDto, Customer>();
+            CreateMap<CustomerUpdateDto, Customer>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            CreateMap<Customer, CustomerDto>()
+                .ForMember(dest => dest.OrderCount, opt => opt.MapFrom(src => src.Orders.Count));
+        }
+    }
+}
