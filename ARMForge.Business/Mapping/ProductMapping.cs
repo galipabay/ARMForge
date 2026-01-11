6768b35cf020a7e47d1606af9ff3799773f5380f
galipabay <galipabay@GALIPSSTARSHIP>
2026-01-12 00:09:06 +0300

    refactor(mapping): isolate Product mapping and implement strict pre-conditions for updates

---
+++ ARMForge.Business/Mapping/ProductMapping.cs
@@ -0,0 +1,33 @@
+﻿using ARMForge.Kernel.Entities;
+using ARMForge.Types.DTOs;
+using AutoMapper;
+
+namespace ARMForge.Business.Mapping
+{
+    public class ProductMapping : Profile
+    {
+        public ProductMapping()
+        {
+            CreateMap<ProductCreateDto, Product>();
+
+            CreateMap<ProductUpdateDto, Product>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UnitPrice,
+                    opt => opt.PreCondition(src => src.UnitPrice.HasValue))
+                .ForMember(dest => dest.StockQuantity,
+                    opt => opt.PreCondition(src => src.StockQuantity.HasValue))
+                .ForMember(dest => dest.Name,
+                    opt => opt.PreCondition(src => !string.IsNullOrEmpty(src.Name)))
+                .ForMember(dest => dest.StockKeepingUnit,
+                    opt => opt.PreCondition(src => !string.IsNullOrEmpty(src.StockKeepingUnit)))
+                .ForMember(dest => dest.Description,
+                    opt => opt.PreCondition(src => src.Description != null))
+                .ForMember(dest => dest.Category,
+                    opt => opt.PreCondition(src => !string.IsNullOrEmpty(src.Category)));
+
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.OrderItemCount,
+                    opt => opt.MapFrom(src => src.OrderItems.Count));
+        }
+    }
+}
