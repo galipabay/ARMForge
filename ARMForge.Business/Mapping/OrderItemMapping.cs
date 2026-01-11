92c4e9c2b6ed6943d8675be0070c2f1e07cf3a9a
galipabay <galipabay@GALIPSSTARSHIP>
2026-01-12 00:10:10 +0300

    refactor(mapping): finalize mapping decoupling with OrderItemMapping and strict field protection

---
+++ ARMForge.Business/Mapping/OrderItemMapping.cs
@@ -0,0 +1,40 @@
+﻿using ARMForge.Kernel.Entities;
+using ARMForge.Types.DTOs;
+using AutoMapper;
+
+namespace ARMForge.Business.Mapping
+{
+    public class OrderItemMapping : Profile
+    {
+        public OrderItemMapping()
+        {
+            CreateMap<OrderItem, OrderItemDto>()
+                .ForMember(dest => dest.ProductName, opt => opt.MapFrom(src => src.Product.Name));
+
+            CreateMap<OrderItemCreateDto, OrderItem>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Subtotal, opt => opt.Ignore())
+                .ForMember(dest => dest.Weight, opt => opt.Ignore())
+                .ForMember(dest => dest.Volume, opt => opt.Ignore())
+                .ForMember(dest => dest.Order, opt => opt.Ignore())
+                .ForMember(dest => dest.Product, opt => opt.Ignore());
+
+            CreateMap<OrderItemUpdateDto, OrderItem>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Subtotal, opt => opt.Ignore())
+                .ForMember(dest => dest.Weight, opt => opt.Ignore())
+                .ForMember(dest => dest.Volume, opt => opt.Ignore())
+                .ForMember(dest => dest.Order, opt => opt.Ignore())
+                .ForMember(dest => dest.Product, opt => opt.Ignore())
+                .ForAllMembers(opt =>
+                {
+                    opt.Condition((src, dest, srcMember) =>
+                    {
+                        if (srcMember == null) return false;
+                        if (srcMember is string s) return !string.IsNullOrWhiteSpace(s);
+                        return true;
+                    });
+                });
+        }
+    }
+}
