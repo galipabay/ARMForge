fba35875a6f0e446ce5f9b9bea60ec3551c48fce
galipabay <galipabay@GALIPSSTARSHIP>
2026-01-12 00:10:28 +0300

    refactor(mapping): add DriverMapping with secure user relationship handling

---
+++ ARMForge.Business/Mapping/DriverMapping.cs
@@ -0,0 +1,28 @@
+﻿using ARMForge.Kernel.Entities;
+using ARMForge.Types.DTOs;
+using AutoMapper;
+
+namespace ARMForge.Business.Mapping
+{
+    public class DriverMapping : Profile
+    {
+        public DriverMapping()
+        {
+            CreateMap<DriverCreateDto, Driver>()
+                .ForMember(dest => dest.User, opt => opt.Ignore());
+
+            CreateMap<DriverUpdateDto, Driver>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.User, opt => opt.Ignore());
+
+            CreateMap<Driver, DriverDto>()
+                .ForMember(dest => dest.UserFullName, opt => opt
+                    .MapFrom(src => src.User != null ? src.User.Firstname + " " + src.User.Lastname : null))
+                .ForMember(dest => dest.ShipmentCount,
+                    opt => opt.MapFrom(src => src.Shipments != null ? src.Shipments.Count : 0));
+
+
+        }
+    }
+}
