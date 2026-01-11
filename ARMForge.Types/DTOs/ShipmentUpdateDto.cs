2509af121a542c6cbb6926419cb70e1d8281bfe0
galipabay <galipabay@GALIPSSTARSHIP>
2026-01-12 00:01:41 +0300

    feat & refactor(Shipment): support driver/vehicle reassignment and implement enum-based status in ShipmentUpdateDto

--- ARMForge.Types/DTOs/ShipmentUpdateDto.cs
+++ ARMForge.Types/DTOs/ShipmentUpdateDto.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ARMForge.Kernel.Enums;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,18 +10,19 @@ namespace ARMForge.Types.DTOs
 {
     public class ShipmentUpdateDto
     {
-        // Güncellenebilir shipment alanları
-        public string? Status { get; set; }
-        public string? Origin { get; set; }  // İstenirse başlangıç noktası değiştirilebilir
+        [MaxLength(128)]
+        public string? Origin { get; set; }
+
+        [MaxLength(128)]
         public string? Destination { get; set; }
+
         public DateTimeOffset? DepartureDate { get; set; }
         public DateTimeOffset? EstimatedDeliveryDate { get; set; }
         public DateTimeOffset? ActualDeliveryDate { get; set; }
 
-        // Opsiyonel olarak sürücü ve araç durumu değiştirilebilir
-        public bool? IsDriverOnDuty { get; set; }
-        public bool? IsDriverAvailable { get; set; }
-        public bool? IsVehicleAvailable { get; set; }
-    }
+        public ShipmentStatus? Status { get; set; }
 
+        public int? DriverId { get; set; }
+        public int? VehicleId { get; set; }
+    }
 }
