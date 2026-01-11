cdba60690a9607c07ca50846a2b815f4fb775f2e
galipabay <galipabay@GALIPSSTARSHIP>
2026-01-12 00:09:19 +0300

    refactor(mapping): secure Order mapping by ignoring internal fields and implementing strict update conditions

---
+++ ARMForge.Business/Mapping/OrderMapping.cs
@@ -0,0 +1,53 @@
+﻿using ARMForge.Kernel.Entities;
+using ARMForge.Types.DTOs;
+using AutoMapper;
+using System;
+
+namespace ARMForge.Business.Mapping
+{
+    public class OrderMapping : Profile
+    {
+        public OrderMapping()
+        {
+            CreateMap<Order, OrderDto>()
+                .ForMember(dest => dest.CustomerCompanyName,
+                    opt => opt.MapFrom(src => src.Customer.CompanyName))
+                .ForMember(dest => dest.Shipments,
+                    opt => opt.MapFrom(src => src.Shipments));
+
+            CreateMap<OrderCreateDto, Order>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.Ignore())
+                .ForMember(dest => dest.PaymentStatus, opt => opt.Ignore())
+                .ForMember(dest => dest.TotalAmount, opt => opt.Ignore())
+                .ForMember(dest => dest.TotalWeight, opt => opt.Ignore())
+                .ForMember(dest => dest.TotalVolume, opt => opt.Ignore())
+                .ForMember(dest => dest.Customer, opt => opt.Ignore())
+                .ForMember(dest => dest.Shipments, opt => opt.Ignore())
+                .ForMember(dest => dest.OrderItems, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(_ => DateTime.UtcNow));
+
+            CreateMap<OrderUpdateDto, Order>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.Ignore())
+                .ForMember(dest => dest.PaymentStatus, opt => opt.Ignore())
+                .ForMember(dest => dest.TotalAmount, opt => opt.Ignore())
+                .ForMember(dest => dest.TotalWeight, opt => opt.Ignore())
+                .ForMember(dest => dest.TotalVolume, opt => opt.Ignore())
+                .ForMember(dest => dest.Customer, opt => opt.Ignore())
+                .ForMember(dest => dest.Shipments, opt => opt.Ignore())
+                .ForMember(dest => dest.OrderItems, opt => opt.Ignore())
+                .ForMember(dest => dest.CustomerId, opt => opt.Condition(src => src.CustomerId.HasValue))
+                .ForMember(dest => dest.RequiredDate, opt => opt.Condition(src => src.RequiredDate.HasValue))
+                .ForAllMembers(opt =>
+                {
+                    opt.Condition((src, dest, srcMember) =>
+                    {
+                        if (srcMember == null) return false;
+                        if (srcMember is string s) return !string.IsNullOrWhiteSpace(s);
+                        return true;
+                    });
+                });
+        }
+    }
+}
