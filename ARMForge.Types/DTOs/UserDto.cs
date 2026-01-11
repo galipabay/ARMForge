4be41b82af77cc3221edb9ba2f44dab095d59f89
galipabay <galipabay@GALIPSSTARSHIP>
2026-01-12 00:00:52 +0300

    refactor(dto): modernize UserDto with C# 11 required members and C# 12 collection expressions

--- ARMForge.Types/DTOs/UserDto.cs
+++ ARMForge.Types/DTOs/UserDto.cs
@@ -9,15 +9,15 @@ namespace ARMForge.Types.DTOs
     public class UserDto
     {
         public int Id { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public string Email { get; set; }
+        public required string Firstname { get; set; }
+        public required string Lastname { get; set; }
+        public required string Email { get; set; }
         public string? PhoneNumber { get; set; }
         public DateTimeOffset? LastLogin { get; set; }
         public DateTimeOffset? HireDate { get; set; }
         public string? Department { get; set; }
         public int? ManagerId { get; set; }
         public decimal Salary { get; set; }
-        public ICollection<UserRoleDto> UserRoles { get; set; } = new List<UserRoleDto>();
+        public ICollection<UserRoleDto> UserRoles { get; set; } = [];
     }
 }
