52c4449d315bc950c80c995b29862d6812dacac0
galipabay <galipabay@GALIPSSTARSHIP>
2026-01-12 00:00:21 +0300

    refactor(dto): optimize UserRoleDto by removing nested objects to reduce payload size

--- ARMForge.Types/DTOs/UserRoleDto.cs
+++ ARMForge.Types/DTOs/UserRoleDto.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ARMForge.Types.DTOs
+﻿namespace ARMForge.Types.DTOs
 {
     public class UserRoleDto
     {
         public int UserId { get; set; }
-        public UserDto User { get; set; }
-
         public int RoleId { get; set; }
-        public RoleDto Role { get; set; }
     }
 }
+
